b3ab48176dbdc65a589da686cc953eed4ee01ac0
itthiphon piyaphan <ittphon1995@gmail.com>
2022-02-01 12:48:36 +0700

    Fix: path css

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@ import { defineNuxtConfig } from 'nuxt3'
 
 // https://v3.nuxtjs.org/docs/directory-structure/nuxt.config
 export default defineNuxtConfig({
-  css: ['~/assets/styles.scss'],
+  css: ['~/assets/scss/styles.scss'],
   build: {
     postcss: {
       postcssOptions: {
